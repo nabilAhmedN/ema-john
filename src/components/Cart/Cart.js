6226a962f417ed07b447b002dace978122b86302
nabilAhmedN <nabilahmednahid@gmail.com>
2022-10-05 11:05:25 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+    
+    const {cart} = props;
+    let total = 0;
+    let shipping = 0;
+
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+        // console.log(total)
+    }
+
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
+
+    return (
+        <div className='cart'><h2>cart</h2>
+           <p>Selected Items: {cart.length}</p>
+           <p>Total Price: ${total}</p>
+           <p>Total Shipping: ${shipping}</p>
+           <p>Tax: {tax}</p>
+           <h5>Grand Total: {grandTotal}</h5>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    // console.log(props.product)
+    const { name, img, seller, price, ratings} = props.product;
+
+    
+    return (
+        <div className='product'>
+            <img src={img} alt="" srcset="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} Stars</small></p>
+            </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
